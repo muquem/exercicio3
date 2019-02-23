8e1d2b584e6f1e614ba1cf54fa31ae74c8dee30f
Your NameMaikon <maikonsysten@hotmail.com>
2019-02-22 22:03:55 -0300

    Exercício de revisão 22/02/2019

---
+++ src/br/com/etechoracio/sihas/biblioteca/model/Autor.java
@@ -0,0 +1,32 @@
+package br.com.etechoracio.sihas.biblioteca.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name="TBL_AUTOR")
+public class Autor {
+	@Id
+	@GeneratedValue
+	@Column(name="ID_AUTOR")
+	private Long id;
+
+	@Column(name="TX_NOME")
+	private String nome;
+
+ @Column(name="DT_INICIO_VIGENCIA")
+	private Date dataInicio;
+
+@Column(name="DT_FIM_VIGENCIA")
+	private Date dataFim;
+}
---
+++ src/br/com/etechoracio/sihas/biblioteca/view/ValidadorMB.java
@@ -0,0 +1,35 @@
+package br.com.etechoracio.sihas.biblioteca.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.apache.commons.lang.exception.ExceptionUtils;
+
+import br.com.etechoracio.sihas.biblioteca.model.Autor;
+
+
+@ManagedBean
+public class ValidadorMB {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public void doValida() {
+		try {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("BIBLIO_MYSQL");
+			EntityManager em = emf.createEntityManager();
+			em.find(Autor.class, 1L);
+			message = "Conexão OK";
+		} catch (Exception e) {
+			message = ExceptionUtils.getFullStackTrace(e);
+			message = message.replaceAll("\\n", "<br/>");
+		}
+	}
+}
